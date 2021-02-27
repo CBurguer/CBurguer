b875b1fa6876c7929bcd6e6142a52186f4ef5a4c
Pedro Parimoski <pedroparimoski@hotmail.com>
2021-02-26 22:29:14 -0300

    pedro btn ok

--- public/assets/scripts/orders.js
+++ public/assets/scripts/orders.js
@@ -1,9 +1,29 @@
-let btnExcluir = document.querySelectorAll(".excluir");
-let liExc = document.querySelector(".liex");
+let btnLixeira = document.querySelectorAll(".excluir");
+let btnCancelar = document.querySelectorAll(".btncan");
+let btnExcluir = document.querySelectorAll(".btnex");
+
+btnLixeira.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let liAct = btn.closest("li");
+    let liDel = liAct.querySelector(".liex");
+    liDel.classList.add("ex-open");
+  });
+});
+
+//console.log(btnCancelar);
+
+btnCancelar.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let liCan = e.target.parentElement;
+    liCan.classList.remove("ex-open");
+  });
+});
 
 btnExcluir.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    liExc.classList.add("ex-open");
-    console.dir(e.target.parentElement);
+    let liAct = e.target.parentElement;
+    let ulList = liAct.closest("ul");
+    let liPai = ulList.closest("li");
+    liPai.remove();
   });
 });
