b0971e476b61a9c556336fd9740b5f0073f5bf6a
Guilherme Ferreti <guiferretis@gmail.com>
2021-03-03 20:13:30 -0300

    index

--- public/assets/scripts/utils.js
+++ public/assets/scripts/utils.js
@@ -1,9 +1,64 @@
 export function getFormValues(form) {
-  const values = {};
+    const values = {};
 
-  form.querySelectorAll("[name]").forEach((input) => {
-    values[input.name] = input.value;
-  });
+    form.querySelectorAll("[name]").forEach((input) => {
+        values[input.name] = input.value;
+    });
 
-  return values;
+    return values;
+}
+
+export function appendToTemplate(element, tagName, html) 
+{
+    const wrappElement = document.createElement(tagName);
+
+    wrappElement.innerHTML = html;
+
+    element.append(wrappElement);
+
+    return wrappElement;
+}
+
+export function calculateBurgerTotal(burguer) 
+{
+    let total = 0;
+
+    burguer.ingredients.forEach(ingredient => {
+
+        total = total + Number(ingredient.price)
+    });
+
+    total = total + Number(burguer.bread.price);
+
+    return total;
+}
+
+export function calculateTrayTotal(tray)
+{
+    let total = 0;
+
+    tray.forEach(burguer => {
+
+        total = total + calculateBurgerTotal(burguer);
+    });
+
+    return total;
+}
+
+export function formatCurrency(value) 
+{
+    // return parseFloat(value).toLocaleString('pt-br', {
+    //     style: 'currency',
+    //     currency: 'BRL'
+    // });
+
+    return Intl.NumberFormat('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(value);
+}
+
+export function showAlertError(message) {
+
+    alert(message);
 }
