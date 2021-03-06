ac52a15aa8555a961eafc1fdfafcb8bb0d8ed4ed
Guilherme Ferreti <guiferretis@gmail.com>
2021-03-05 23:25:08 -0300

    utils.js alterado

--- public/assets/scripts/utils.js
+++ public/assets/scripts/utils.js
@@ -14,3 +14,45 @@ export function formatCurrency(value) {
     currency: 'BRL'
   }).format(value)
 }
+
+export function appendToTemplate(element, tagName, html) 
+{
+  const wrappElement = document.createElement(tagName);
+
+  wrappElement.innerHTML = html;
+
+  element.append(wrappElement);
+
+  return wrappElement;
+}
+
+export function calculateBurgerTotal(burguer) 
+{
+  let total = 0;
+
+  burguer.ingredients.forEach(ingredient => {
+
+      total = total + Number(ingredient.price)
+  });
+
+  total = total + Number(burguer.bread.price);
+
+  return total;
+}
+
+export function calculateTraySubTotal(tray)
+{
+  let total = 0;
+
+  tray.forEach(burguer => {
+
+      total = total + calculateBurgerTotal(burguer);
+  });
+
+  return total;
+}
+
+export function showAlertError(message) {
+
+  alert(message);
+}
