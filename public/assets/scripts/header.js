be425556cb3be91600ee838a17f942f023ff7d22
Christian <christiandelilomoreira@hotmail.com>
2021-03-11 22:34:58 -0300

    Exibido no aside e-mail do usuário autenticado

--- public/assets/scripts/header.js
+++ public/assets/scripts/header.js
@@ -1,3 +1,5 @@
+import firebase from './firebase-app'
+
 var header = document.querySelector(".header");
 
 if (header) {
@@ -5,6 +7,13 @@ if (header) {
   var open = document.querySelector(".open-menu");
   var close = document.querySelector(".close-menu");
   var contact = document.querySelector("#contact-link");
+  var email = document.querySelector('.email');
+
+  firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        email.innerHTML = user.email
+      }
+  })
 
   if (open) {
     open.addEventListener("click", () => {
